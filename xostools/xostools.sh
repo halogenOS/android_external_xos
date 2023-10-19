e16a3ad38d707f3fbe1535f5fd00231a7a134aa1
Simão Gomes Viana <devel@superboring.dev>
2023-10-20 00:06:49 +0200

    Update license headers

--- xostools/xostools.sh
+++ xostools/xostools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (C) 2016-2022 The halogenOS Project
+# Copyright (C) 2016-2023 The halogenOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
