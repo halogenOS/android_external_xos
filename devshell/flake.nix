80e94a8659728cc2ae61437f1ff9d1bd913e8805
Simão Gomes Viana <devel@superboring.dev>
2025-06-24 00:15:17 +0200

    devshell: add formatter, add cuttlefish-common stuff, format

--- devshell/flake.nix
+++ devshell/flake.nix
@@ -3,106 +3,162 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
-  outputs = { nixpkgs, ... }:
-  let
-    forEachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    lib = nixpkgs.lib;
-    fhs = name: pkgs: attrs: pkgs.buildFHSEnvBubblewrap ({
-        inherit name;
-        targetPkgs = pkgs: with pkgs; [
-          bc
-          bison
-          ccache
-          clang_20
-          clangStdenv
-          elfutils elfutils.dev
-          flex
-          fontconfig
-          freetype
-          gcc
-          git git-lfs
-          git-repo
-          glibc glibc.dev
-          gnumake
-          go
-          imagemagick
-          libbsd.dev
-          libelf
-          libgcc
-          lld_20
-          llvm_20
-          libxcrypt-legacy
-          ncurses5 ncurses ncurses.dev
-          openssl openssl.dev
-          perl
-          pkgconf
-          pngcrush
-          python3
-          roboto
-          rsync
-          unzip
-          util-linux
-          xmlstarlet
-          zip
-          zlib
-          zsh
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      lib = nixpkgs.lib;
+      fhs =
+        name: pkgs: attrs:
+        pkgs.buildFHSEnvBubblewrap (
+          {
+            inherit name;
+            targetPkgs =
+              pkgs:
+              with pkgs;
+              [
+                bc
+                bison
+                ccache
+                clang_20
+                clangStdenv
+                elfutils
+                elfutils.dev
+                flex
+                fontconfig
+                freetype
+                gcc
+                git
+                git-lfs
+                git-repo
+                glibc
+                glibc.dev
+                gnumake
+                go
+                imagemagick
+                libbsd.dev
+                libelf
+                libgcc
+                lld_20
+                llvm_20
+                libxcrypt-legacy
+                ncurses5
+                ncurses
+                ncurses.dev
+                openssl
+                openssl.dev
+                perl
+                pkgconf
+                pngcrush
+                python3
+                roboto
+                rsync
+                unzip
+                util-linux
+                xmlstarlet
+                zip
+                zlib
+                zsh
 
-          # misc packages
-          payload-dumper-go
-          strace
+                # misc packages
+                payload-dumper-go
+                strace
 
-          # for emulator
-          libpulseaudio
-          libpng
-          nss
-          nspr
-          expat
-          libdrm
-          libbsd
-          xcb-util-cursor
-          libxkbcommon
-          libsForQt5.qt5.qtwayland
-          xwayland
-        ] ++ (with pkgs.xorg; [
-          libX11
-          libXcursor
-          libxcb
-          xcbutilimage
-          xcbutilwm
-          xcbutilkeysyms
-          xcbutilrenderutil
-          libXi
-          libXext
-          libxkbfile
-          libSM
-          libICE
-        ]);
-        profile = builtins.readFile ((pkgs.formats.keyValue {}).generate "" (
-          lib.mapAttrs' (name: value: { name = "export ${name}"; inherit value; }) {
-            LIBGCC_DIR = "${pkgs.libgcc.out}/lib/gcc/${pkgs.libgcc.stdenv.buildPlatform.config}/${pkgs.libgcc.version}";
-            FONTCONFIG_FILE = with pkgs; makeFontsConf { fontDirectories = [ roboto ]; };
-            LD_LIBRARY_PATH="/usr/lib64";
+                # for emulator
+                libpulseaudio
+                libpng
+                nss
+                nspr
+                expat
+                libdrm
+                libbsd
+                xcb-util-cursor
+                libxkbcommon
+                libsForQt5.qt5.qtwayland
+                xwayland
+                crosvm
+              ]
+              ++ (with pkgs.xorg; [
+                libX11
+                libXcursor
+                libxcb
+                xcbutilimage
+                xcbutilwm
+                xcbutilkeysyms
+                xcbutilrenderutil
+                libXi
+                libXext
+                libxkbfile
+                libSM
+                libICE
+              ]);
+            profile = builtins.readFile (
+              (pkgs.formats.keyValue { }).generate "" (
+                lib.mapAttrs'
+                  (name: value: {
+                    name = "export ${name}";
+                    inherit value;
+                  })
+                  {
+                    LIBGCC_DIR = "${pkgs.libgcc.out}/lib/gcc/${pkgs.libgcc.stdenv.buildPlatform.config}/${pkgs.libgcc.version}";
+                    FONTCONFIG_FILE = with pkgs; makeFontsConf { fontDirectories = [ roboto ]; };
+                    LD_LIBRARY_PATH = "/usr/lib64";
+                  }
+              )
+            );
+            extraBuildCommands =
+              let
+                cuttlefishCapabilities = pkgs.writeShellScript "" ''
+                  #!${pkgs.runtimeShell}
+                  echo "capability_check"
+                  echo "qemu_cli"
+                  echo "crosvm"
+                  echo -n "vsock"
+                '';
+              in
+              ''
+                mkdir -p $out/usr/lib64/cuttlefish-common/bin
+                ln -s ${cuttlefishCapabilities} $out/usr/lib64/cuttlefish-common/bin/capability_query.py
+              '';
           }
-        ));
-      } // attrs);
-  in rec {
-    packages = forEachSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        direnvShell = (pkgs.mkShell {
-          packages = [
-            (pkgs.writeShellApplication {
-              name = "aosp-env";
-              text = ''nix develop path:external/xos/devshell'';
-            })
-            packages.${system}.execShell
-          ];
-        });
-        execShell = fhs "exec-aosp-env" pkgs {};
-      });
-    devShell = forEachSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in (fhs "aosp-env" pkgs { runScript = "zsh"; }).env
-    );
-  };
+          // attrs
+        );
+    in
+    rec {
+      packages = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          direnvShell = (
+            pkgs.mkShell {
+              packages = [
+                (pkgs.writeShellApplication {
+                  name = "aosp-env";
+                  text = ''nix develop path:external/xos/devshell'';
+                })
+                packages.${system}.execShell
+              ];
+            }
+          );
+          execShell = fhs "exec-aosp-env" pkgs { };
+        }
+      );
+      devShell = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        (fhs "aosp-env" pkgs { runScript = "zsh"; }).env
+      );
+      formatter = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixfmt-rfc-style
+      );
+    };
 }
+
