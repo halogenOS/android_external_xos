b97a79c5825a7fcd1f9be3ef465a0a5478b5826e
Simão Gomes Viana <devel@superboring.dev>
2025-07-12 22:50:51 +0200

    Add new keys

--- xostools/scripts/generate_missing_keys.sh
+++ xostools/scripts/generate_missing_keys.sh
@@ -38,21 +38,26 @@ done
 for apex in \
     com.android.adbd com.android.adservices \
     com.android.adservices.api com.android.appsearch \
+    com.android.appsearch.apk \
     com.android.art com.android.bluetooth \
     com.android.btservices com.android.cellbroadcast \
     com.android.compos com.android.configinfrastructure \
     com.android.connectivity.resources com.android.conscrypt \
     com.android.devicelock com.android.extservices \
-    com.android.graphics.pdf com.android.hardware.biometrics.face.virtual \
+    com.android.graphics.pdf com.android.hardware.authsecret \
+    com.android.hardware.biometrics.face.virtual \
     com.android.hardware.biometrics.fingerprint.virtual \
     com.android.hardware.boot com.android.hardware.cas \
+    com.android.hardware.neuralnetworks com.android.hardware.rebootescrow \
     com.android.hardware.wifi com.android.healthfitness \
     com.android.hotspot2.osulogin com.android.i18n com.android.ipsec \
     com.android.media com.android.media.swcodec \
     com.android.mediaprovider com.android.nearby.halfsheet \
     com.android.networkstack.tethering com.android.neuralnetworks \
+    com.android.nfcservices \
     com.android.ondevicepersonalization com.android.os.statsd \
-    com.android.permission com.android.resolv com.android.rkpd \
+    com.android.permission com.android.profiling \
+    com.android.resolv com.android.rkpd \
     com.android.runtime com.android.safetycenter.resources \
     com.android.scheduling com.android.sdkext \
     com.android.support.apexer com.android.telephony \
